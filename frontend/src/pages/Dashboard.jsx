b32ebfb4ec400fb255f9d3a0750fafa921f57890
Allen <allenliu0476@gmail.com>
2024-11-12 16:48:18 -0500

    Added alert to indicate success or failure on Display name update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,73 @@
-import { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUser } from '@clerk/clerk-react';
 import Sidebar from '../components/Sidebar';
-import EventCreate from '../components/EventCreate';
+
+// Memoize the UserProfile component to avoid unnecessary re-renders
+const UserProfile = React.memo(({
+    userInfo,
+    isFormOpen,
+    setIsFormOpen,
+    handleDisplayNameChange,
+    newDisplayName,
+    setNewDisplayName
+}) => (
+    <div className="flex flex-col items-center p-6 bg-white shadow-lg rounded-md">
+        <div className="flex gap-6 items-center">
+            <img
+                src={userInfo?.imageUrl}
+                alt="User Avatar"
+                className="rounded-full w-24 h-24 object-cover"
+            />
+            <div className="flex flex-col py-3">
+                <h3 className="text-xl font-bold">{userInfo?.username}</h3>
+                <p className="text-gray-500">Buddy ID: {userInfo?.id}</p>
+                {/* Button to toggle visibility of the form */}
+                <button
+                    onClick={() => setIsFormOpen(!isFormOpen)}
+                    className="mt-4 px-4 py-2 text-sm text-white bg-blue-500 rounded-md hover:bg-blue-600"
+                >
+                    {isFormOpen ? 'Cancel' : 'Change Display Name'}
+                </button>
+            </div>
+        </div>
+
+        {/* Conditionally render the form to change the display name */}
+        {isFormOpen && (
+            <div className="flex-col mt-4 w-full max-w-sm space-y-4">
+                <form onSubmit={handleDisplayNameChange}>
+                    <input
+                        type="text"
+                        value={newDisplayName}
+                        onChange={(e) => setNewDisplayName(e.target.value)}
+                        className="w-full p-3 border border-gray-300 rounded-md"
+                        placeholder="Enter new display name"
+                        required
+                    />
+                    <div className="flex justify-center mt-6">
+                        <button
+                            type="submit"
+                            className="w-1/2 bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 mb-4"
+                            disabled={!newDisplayName}
+                        >
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </div>
+        )}
+    </div>
+));
 
 const Dashboard = () => {
+    //User states
     const { isSignedIn, user } = useUser();
     const [userInfo, setUserInfo] = useState(null);
-    const [isFormOpen, setIsFormOpen] = useState(false);
     const [newDisplayName, setNewDisplayName] = useState('');
-    const formRef = useRef(null);
-    const inputRef = useRef(null);  // Ref to focus input when form is opened
-    const debouncedDisplayName = useRef(''); // Store the input value to debounce
+
+    //Form states
+    const [isFormOpen, setIsFormOpen] = useState(false);
+    const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
         if (isSignedIn && user) {
@@ -45,11 +102,17 @@ const Dashboard = () => {
                 displayName: data.user.displayName,
             });
 
-            setNewDisplayName(''); // Clear the input field
-            setIsFormOpen(false); // Close the form
+            setSuccessMessage('Display name updated successfully!'); // Show success message
 
+            setNewDisplayName('');
+            setIsFormOpen(false);
+
+            setTimeout(() => setSuccessMessage(''), 3000);
         } catch (error) {
             console.error('Error updating display name:', error);
+            setErrorMessage('Failed to update display name. Please try again.');
+
+            setTimeout(() => setErrorMessage(''), 3000);
         }
     };
 
@@ -58,90 +121,40 @@ const Dashboard = () => {
         return <div>You are not logged in.</div>;
     }
 
-    const UserProfile = ({ userInfo }) => (
-        <div className="flex flex-col items-center p-6 bg-white shadow-lg rounded-md">
-            <div className="flex gap-6 items-center">
-                <img
-                    src={userInfo?.imageUrl}
-                    alt="User Avatar"
-                    className="rounded-full w-24 h-24 object-cover"
-                />
-                <div className="flex flex-col py-3">
-                    <h3 className="text-xl font-bold">{userInfo?.username}</h3>
-                    <p className="text-gray-500">Buddy ID: {userInfo?.id}</p>
-                    {/* Button to change displayName */}
-                    <button
-                        onClick={() => {
-                            setIsFormOpen(true);
-                            setNewDisplayName(userInfo?.displayName || ''); // Initialize input with current displayName
-                            debouncedDisplayName.current = userInfo?.displayName || ''; // Store current display name in ref
-                        }}
-                        className="mt-4 px-4 py-2 text-sm text-white bg-blue-500 rounded-md hover:bg-blue-600"
-                    >
-                        Change Display Name
-                    </button>
-                </div>
-            </div>
-
-            {/* Conditionally render the form to change the display name */}
-            {isFormOpen && (
-                <form
-                    ref={formRef}
-                    onSubmit={handleDisplayNameChange}
-                    className="mt-4 w-full max-w-sm space-y-4"
-                >
-                    <input
-                        ref={inputRef}  // Focus the input automatically when the form is opened
-                        type="text"
-                        value={newDisplayName}
-                        onChange={(e) => {
-                            setNewDisplayName(e.target.value); // Update the display name state on input change
-                            debouncedDisplayName.current = e.target.value; // Store input value in ref for debouncing
-                        }}
-                        className="w-full p-3 border border-gray-300 rounded-md"
-                        placeholder="Enter new display name"
-                        required
-                    />
-                    <div className="flex justify-between">
-                        <button
-                            type="submit"
-                            className="w-1/2 bg-green-500 text-white py-2 rounded-md hover:bg-green-600"
-                        >
-                            Save
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => setIsFormOpen(false)}
-                            className="w-1/2 bg-gray-500 text-white py-2 rounded-md hover:bg-gray-600"
-                        >
-                            Cancel
-                        </button>
-                    </div>
-                </form>
-            )}
-        </div>
-    );
-
-    // Focus the input when the form is opened
-    useEffect(() => {
-        if (isFormOpen && inputRef.current) {
-            inputRef.current.focus();
-        }
-    }, [isFormOpen]);
-
     return (
         <>
             <Sidebar />
             <div className="ml-60 p-20 flex justify-center">
                 <div className="max-w-3xl w-full">
                     <h1 className="text-3xl text-gray-700 font-bold mb-6">Welcome Back, {userInfo?.username}</h1>
+
+                    {/* Display success or error message */}
+                    {successMessage && (
+                        <div className="bg-green-500 text-white py-2 px-4 rounded-md text-center mb-4">
+                            {successMessage}
+                        </div>
+                    )}
+
+                    {errorMessage && (
+                        <div className="bg-red-500 text-white py-2 px-4 rounded-md text-center mb-4">
+                            {errorMessage}
+                        </div>
+                    )}
+
                     {!userInfo ? (
                         <div className="flex justify-center items-center py-4">
                             <span className="loader"></span>
                             <p className="ml-2 text-gray-500">Loading your profile...</p>
                         </div>
                     ) : (
-                        <UserProfile userInfo={userInfo} />
+                        <UserProfile
+                            userInfo={userInfo}
+                            isFormOpen={isFormOpen}
+                            setIsFormOpen={setIsFormOpen}
+                            handleDisplayNameChange={handleDisplayNameChange}
+                            newDisplayName={newDisplayName}
+                            setNewDisplayName={setNewDisplayName}
+                        />
                     )}
                 </div>
             </div>
